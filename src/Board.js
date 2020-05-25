035501061ec3df46233f18a45168e3529dbde8a1
Karen Liu <karenliu1@gmail.com>
2020-05-25 14:14:01 -0400

    Add a sliding animation.

--- src/Board.js
+++ src/Board.js
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import React from 'react';
 import styles from './Board.module.scss';
 import backgroundImage from './images/bg.jpg';
@@ -14,10 +15,16 @@ function generateBoard(dimension) {
   return board;
 }
 
+const ANIMATION_MS = 500;
+
 export default function Board({ dimension }) {
   let [board, setBoard] = React.useState(generateBoard(dimension));
   let [blankRow, setBlankRow] = React.useState(dimension - 1);
   let [blankCol, setBlankCol] = React.useState(dimension - 1);
+
+  let [animation, setAnimation] = React.useState(null);
+  let [animatingTileRow, setAnimatingTileRow] = React.useState(null);
+  let [animatingTileCol, setAnimatingTileCol] = React.useState(null);
 
   const swapTiles = React.useCallback(
     (row1, col1, row2, col2) => {
@@ -29,24 +36,40 @@ export default function Board({ dimension }) {
     [board]
   );
 
-  const onClick = React.useCallback(
-    (row, col) => {
-      if (row - 1 === blankRow && col === blankCol) {
-        swapTiles(row, col, row - 1, col);
-      } else if (row + 1 === blankRow && col === blankCol) {
-        swapTiles(row, col, row + 1, col);
-      } else if (row === blankRow && col - 1 === blankCol) {
-        swapTiles(row, col, row, col - 1);
-      } else if (row === blankRow && col + 1 === blankCol) {
-        swapTiles(row, col, row, col + 1);
-      } else {
-        // Not adjacent to blank space, do nothing
-        return;
-      }
-      setBlankRow(row);
-      setBlankCol(col);
+  const moveTile = React.useCallback(
+    (row, col, animation) => {
+      setAnimatingTileRow(row);
+      setAnimatingTileCol(col);
+      setAnimation(animation);
+      setTimeout(() => {
+        setAnimatingTileRow(null);
+        setAnimatingTileCol(null);
+        setAnimation(null);
+
+        swapTiles(row, col, blankRow, blankCol);
+        setBlankRow(row);
+        setBlankCol(col);
+      }, ANIMATION_MS);
     },
     [swapTiles, blankRow, blankCol]
+  );
+
+  const onClick = React.useCallback(
+    (row, col) => {
+      if (animation) return; // Ignore clicks during animation
+
+      if (row - 1 === blankRow && col === blankCol) {
+        moveTile(row, col, styles.slideUp);
+      } else if (row + 1 === blankRow && col === blankCol) {
+        moveTile(row, col, styles.slideDown);
+      } else if (row === blankRow && col - 1 === blankCol) {
+        moveTile(row, col, styles.slideLeft);
+      } else if (row === blankRow && col + 1 === blankCol) {
+        moveTile(row, col, styles.slideRight);
+      }
+      // Not adjacent to blank space, do nothing
+    },
+    [moveTile, animation, blankCol, blankRow]
   );
 
   const backgroundSize = 100 * dimension;
@@ -66,7 +89,16 @@ export default function Board({ dimension }) {
 
             return (
               <div
-                className={styles.tile}
+                className={cn([
+                  styles.tile,
+                  {
+                    [styles.nonEmpty]: tile !== 0,
+                    [animation]:
+                      animation &&
+                      animatingTileRow === row &&
+                      animatingTileCol === col
+                  }
+                ])}
                 key={col}
                 onClick={() => onClick(row, col)}
                 style={
