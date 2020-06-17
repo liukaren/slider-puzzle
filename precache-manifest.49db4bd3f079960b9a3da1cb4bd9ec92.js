03ead0e1dcd4cc61f750ce4a7d028e9c9d4b400b
Karen Liu <karenliu1@gmail.com>
2020-06-17 01:27:26 -0400

    Updates

---
+++ precache-manifest.49db4bd3f079960b9a3da1cb4bd9ec92.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e27e2c6b23c647767e72643ae24f4aa",
+    "url": "/slider-puzzle/index.html"
+  },
+  {
+    "revision": "0453605d8b4a1778fd12",
+    "url": "/slider-puzzle/static/css/main.f30f56f6.chunk.css"
+  },
+  {
+    "revision": "ef45a7b8f1a758db013e",
+    "url": "/slider-puzzle/static/js/2.7384d79c.chunk.js"
+  },
+  {
+    "revision": "554c5117578f42ee6eb4f89c6db2e154",
+    "url": "/slider-puzzle/static/js/2.7384d79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0453605d8b4a1778fd12",
+    "url": "/slider-puzzle/static/js/main.7dc36daa.chunk.js"
+  },
+  {
+    "revision": "0f786b75fc1f6689b02e",
+    "url": "/slider-puzzle/static/js/runtime-main.96e6c013.js"
+  },
+  {
+    "revision": "1f4134eef0475dc7a94f1ecc7d3869b9",
+    "url": "/slider-puzzle/static/media/collapse.1f4134ee.svg"
+  },
+  {
+    "revision": "800d2899ff77a0fa0fe3da62a406aac4",
+    "url": "/slider-puzzle/static/media/expand.800d2899.svg"
+  },
+  {
+    "revision": "ec5bc1b3148ca464a3a3e17af89c3ead",
+    "url": "/slider-puzzle/static/media/github.ec5bc1b3.svg"
+  },
+  {
+    "revision": "f676b5521b754b6293440ac1285cf147",
+    "url": "/slider-puzzle/static/media/lightbulb-fill.f676b552.svg"
+  },
+  {
+    "revision": "35c6c4f7d744541f08be2bbd2770c8db",
+    "url": "/slider-puzzle/static/media/lightbulb-outline.35c6c4f7.svg"
+  },
+  {
+    "revision": "4485f8865ab57c59cc2d7555eb8254a2",
+    "url": "/slider-puzzle/static/media/question.4485f886.svg"
+  },
+  {
+    "revision": "55473c271992498166a1e0682f38d393",
+    "url": "/slider-puzzle/static/media/search.55473c27.svg"
+  },
+  {
+    "revision": "fe2100226016c7273b36a7ada271889b",
+    "url": "/slider-puzzle/static/media/times.fe210022.svg"
+  },
+  {
+    "revision": "dd84ba5663fbb052956d77fcf84fd31c",
+    "url": "/slider-puzzle/static/media/volume-mute.dd84ba56.svg"
+  },
+  {
+    "revision": "cc275a79c690062adfc2ae101824e17b",
+    "url": "/slider-puzzle/static/media/volume-up.cc275a79.svg"
+  }
+]);
